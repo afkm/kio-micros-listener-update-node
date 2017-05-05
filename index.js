63c2cd00b5767398870d44563451635008442769
sandbender3000 <christian@afkm.io>
2017-05-05 17:40:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const Path = require('path');
+const _ = require('lodash');
+
+var neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "lalala"));
+
+driver.onCompleted = function() {
+  console.log('Successfully connected to Neo4J');
+};
+
+driver.onError = function(error) {
+  console.log('Neo4J Driver instantiation failed', error);
+};
+
+var session = driver.session();
+
+
+require('seneca')()
+  .use('seneca-amqp-transport')
+  .add('cmd:updateNode,cuid:*,nodeTitle:*,nodeAttributes:*', function(message, done) {
+    var properties = "{ ";
+    properties += "cuid:'" + message.cuid + "'";
+    properties += ", title:'" + message.nodeTitle + "'";
+    _.forOwn(message.nodeAttributes, function(value, key) {
+      properties += ", " + key + ":'" + value + "'";
+    });
+    properties += " }";
+    var queryString = "MATCH (n { cuid:'" + message.cuid + "'}) SET n = " + properties;
+    console.log(queryString);
+    session
+      .run(queryString)
+      .then(function(result) {
+        session.close();
+        var status = "Successfully updated Node " + message.cuid;
+        return done(null, {
+          status
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  })
+  .listen({
+    type: 'amqp',
+    pin: 'cmd:updateNode,cuid:*,nodeTitle:*,nodeAttributes:*',
+    url: process.env.AMQP_URL
+  });
