c87cbf4201be23013aefea0996e32d9c0946c735
sandbender3000 <ctoepfner@react.audio>
2017-06-09 15:03:57 +0200

    changed Neo4J host var to env var NEO4J_HOST

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 
 var neo4j = require('neo4j-driver').v1;
 
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic(process.env.NEO4J_UID, process.env.NEO4J_PWD));
+var driver = neo4j.driver(process.env.NEO4J_HOST, neo4j.auth.basic(process.env.NEO4J_UID, process.env.NEO4J_PWD));
 
 driver.onCompleted = function() {
   console.log('Successfully connected to Neo4J');
